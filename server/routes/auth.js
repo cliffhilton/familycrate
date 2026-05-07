a71c7e171f666a28bc1b561713705e483b789cce
cliffhilton <cliffhilton@gmail.com>
2026-05-07 11:00:33 -0400

    Add register logging

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -21,6 +21,7 @@ router.post("/register", async (req, res) => {
     if (authError) return res.status(400).json({ error: authError.message });
 
     const userId = authData.user.id;
+    console.log("Register: userId =", userId, "email =", email);
 
     // 2. Create Stripe customer
     const customer = await stripe.customers.create({
