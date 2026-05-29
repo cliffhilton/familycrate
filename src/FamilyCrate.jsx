8c16483e2a7d20ad33e8c9a4219431c4dcc59fca
cliffhilton <cliffhilton@gmail.com>
2026-05-29 14:03:10 -0400

    Add zoom 1.15 to app for better readability

--- src/FamilyCrate.jsx
+++ src/FamilyCrate.jsx
@@ -1453,7 +1453,7 @@ function FamilyCrate({ apiData, onLogout }) {
   return (
     <>
       <style>{CSS}</style>
-      <div className="app">
+      <div className="app" style={{zoom:"1.15"}}>
 
         {/* Header */}
         <header className="hdr">
